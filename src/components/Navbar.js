f4a7be114a68ecafa6ba174ed6f0bba0e8b97303
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-02-26 18:27:24 -0500

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className='shadow-md p-4 bg-white'>
-            <div>
+            <div className=''>
                 <h1 className='text-xl font-semibold'>Crypto-Tracker</h1>
             </div>
       </div>
